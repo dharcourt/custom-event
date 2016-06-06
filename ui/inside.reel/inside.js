cc9982e53d57e454ef85d2a68d26f78a1bbf6128
Charles d'Harcourt <noreply>
2016-06-06 01:16:57 +0000

    Update component ui/inside.reel/inside.js

--- ui/inside.reel/inside.js
+++ ui/inside.reel/inside.js
@@ -8,9 +8,25 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Inside = Component.specialize(/** @lends Inside# */ {
+
     constructor: {
-        value: function Inside() {
-            this.super();
+        value: function Inside() {}
+    }
+
+    handleInputEventButtonAction: {
+        value: function (event) {
+            var detail = {message: "Hi from handleInputEventButtonAction()"};
+            console.log("Dispatching \"input\" event with detail: ", detail);
+            this.dispatchEventNamed("input", true, true, detail);
+        }
+    },
+
+    handleNotInputEventButtonAction: {
+        value: function (event) {
+            var detail = {message: "Hi from handleNotInputEventButtonAction()"};
+            console.log("Dispatching \"notInput\" event with details: ", detail);
+            this.dispatchEventNamed("notInput", true, true, detail);
         }
     }
+
 });
