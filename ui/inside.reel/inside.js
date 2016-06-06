07bfdbe793586f2eaf3261073624926f3a7bb0d8
Charles d'Harcourt <noreply>
2016-06-06 01:12:02 +0000

    Add component inside

---
+++ ui/inside.reel/inside.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/inside.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Inside
+ * @extends Component
+ */
+exports.Inside = Component.specialize(/** @lends Inside# */ {
+    constructor: {
+        value: function Inside() {
+            this.super();
+        }
+    }
+});
