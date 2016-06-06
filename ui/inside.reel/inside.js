127cb63404ff0895438750f3cad1f5bcbf0cb4d5
Charles d'Harcourt <noreply>
2016-06-06 01:24:30 +0000

    Update component ui/inside.reel/inside.js

--- ui/inside.reel/inside.js
+++ ui/inside.reel/inside.js
@@ -14,9 +14,17 @@ exports.Inside = Component.specialize(/** @lends Inside# */ {
         }
     },
 
-    handleNotInputEventButtonAction: {
+    handleNotInputEvent1ButtonAction: {
         value: function (event) {
-            var detail = {message: "Hi from handleNotInputEventButtonAction()"};
+            var detail = {message: "Hi from handleNotInputEvent1ButtonAction()"};
+            console.log("Dispatching \"notInput\" event with details: ", detail);
+            this.dispatchEventNamed("notInput", true, true, detail);
+        }
+    },
+
+    handleNotInputEvent2ButtonAction: {
+        value: function (event) {
+            var detail = {message: "Hi from handleNotInputEvent2ButtonAction()"};
             console.log("Dispatching \"notInput\" event with details: ", detail);
             this.dispatchEventNamed("notInput", true, true, detail);
         }
