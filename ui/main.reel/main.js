d8643fd0858b26f4b2e7618a259cec5026e9a4d4
Charles d'Harcourt <noreply>
2016-06-06 01:19:06 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -10,9 +10,7 @@ var Component = require("montage/ui/component").Component;
 exports.Main = Component.specialize(/** @lends Main# */ {
 
     constructor: {
-        value: function Main() {
-            this.super();
-        }
+        value: function Main() {}
     },
 
     handleInput: {
@@ -22,26 +20,10 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         }
     },
 
-    handleInputEventButtonAction: {
-        value: function (event) {
-            var detail = {message: "Hi from handleInputEventButtonAction()"};
-            console.log("Dispatching \"input\" event with detail: ", detail);
-            this.dispatchEventNamed("input", true, true, detail);
-        }
-    },
-
     handleNotInput: {
         value: function (event) {
             console.log("handleNoInput() details: ", event.detail);
             event.preventDefault();
-        }
-    },
-
-    handleNotInputEventButtonAction: {
-        value: function (event) {
-            var detail = {message: "Hi from handleNotInputEventButtonAction()"};
-            console.log("Dispatching \"notInput\" event with details: ", detail);
-            this.dispatchEventNamed("notInput", true, true, detail);
         }
     }
 
